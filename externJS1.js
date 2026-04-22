a2aba419cfc96355837f37830bb2255f10342923
Mohamed Hihi <mohamedhihi361@gmail.com>
2026-04-23 00:01:56 +0200

    Add files via upload

---
+++ externJS1.js
@@ -0,0 +1,21 @@
+
+function addition() {
+		let n1 = parseFloat(document.getElementById("N1").value);
+		let n2 = parseFloat(document.getElementById("N2").value);
+alert("Resultat : " + (n1 + n2));}
+function soustraction() {
+		let n1 = parseFloat(document.getElementById("N1").value);
+		let n2 = parseFloat(document.getElementById("N2").value);
+alert("Resultat : " + (n1 - n2));}
+function multiplication() {
+		let n1 = parseFloat(document.getElementById("N1").value);
+		let n2 = parseFloat(document.getElementById("N2").value);
+alert("Resultat : " + (n1 * n2));}
+function division() {
+		let n1 = parseFloat(document.getElementById("N1").value);
+		let n2 = parseFloat(document.getElementById("N2").value);
+		if (n2!=0){
+alert("Resultat : " + (n1 / n2));}}
+	
+		
+		
